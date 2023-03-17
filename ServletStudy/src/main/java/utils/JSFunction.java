e51fb2caaa93af25ecb550ec7491bf7c55e34d09
ycm7784 <ycm7784@naver.com>
2023-03-17 19:57:42 +0900

    0317

--- ServletStudy/src/main/java/utils/JSFunction.java
+++ ServletStudy/src/main/java/utils/JSFunction.java
@@ -1,21 +1,25 @@
 package utils;
 
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspWriter;
 
 public class JSFunction {
-
+	 // 메시지 알림창을 띄운 후 명시한 URL로 이동합니다.
 	public static void alertLocation(String msg, String url, JspWriter out) {
 		try {
-			String script = ""
+			String script = ""// 삽입할 자바스크립트 코드
 					+"<script>"
 					+"	alert('" + msg + "');"
 					+"	location.href='" + url + "';"
 					+"</script>";
-			out.println(script);
+			out.println(script); //자바스크립트 코드를 out 내장 객체로 출력(삽입)
 					
 		}
 		catch(Exception e) {}
 	}
+	 // 메시지 알림창을 띄운 후 이전 페이지로 돌아갑니다.
 	public static void alertBack(String msg, JspWriter out) {
 		try {
 			String script = ""
@@ -27,4 +31,31 @@ public class JSFunction {
 		}
 		catch(Exception e) {}
 	}
+	 // 메시지 알림창을 띄운 후 명시한 URL로 이동합니다.
+    public static void alertLocation(HttpServletResponse resp, String msg, String url) {
+        try {
+            resp.setContentType("text/html;charset=UTF-8");
+            PrintWriter writer = resp.getWriter();
+            String script = ""
+                          + "<script>"
+                          + "    alert('" + msg + "');"
+                          + "    location.href='" + url + "';"
+                          + "</script>";
+            writer.print(script);
+        }
+        catch (Exception e) {}
+    }
+    // 메시지 알림창을 띄운 후 이전 페이지로 돌아갑니다.
+    public static void alertBack(HttpServletResponse resp, String msg) {
+        try {
+            resp.setContentType("text/html;charset=UTF-8");
+            PrintWriter writer = resp.getWriter();
+            String script = ""
+                          + "<script>"
+                          + "    history.back();"
+                          + "</script>";
+            writer.print(script);
+        }
+        catch (Exception e) {}
+    }
 }
